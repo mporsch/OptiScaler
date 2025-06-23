ee2fb00948c2a32ad0cf393f6f9b5a0d6bf4a117
Marco Porsch <marco.porsch@intenta.de>
2025-06-23 16:39:08 +0200

    fixup! Create utilities for smart-pointer-based COM handling

--- OptiScaler/UtilCom.h
+++ OptiScaler/UtilCom.h
@@ -10,25 +10,22 @@ namespace Detail
 
 // Traits class to determine the pointer type of an std::out_ptr_t or std::inout_ptr_t
 // created via std::out_ptr() or std::inout_ptr()
-template<typename T>
-struct OutInPtrTraits;
+template <typename T> struct OutInPtrTraits;
 
-template<
-    template<typename Smart_, typename Pointer_, typename...> class OutInPtr,
-    typename Smart_,
-    typename Pointer_,
-    typename... Args
->
+// clang-format off: keep the template arguments readable
+template <template <typename Smart_, typename Pointer_, typename...> class OutInPtr,
+          typename Smart_,
+          typename Pointer_,
+          typename... Args>
 struct OutInPtrTraits<OutInPtr<Smart_, Pointer_, Args...>>
 {
     using Smart = Smart_;
     using Pointer = Pointer_;
 };
-
+// clang-format on
 
 // Compare combaseapi.h IID_PPV_ARGS_Helper
-template<class OutInPtr>
-OutInPtr&& IID_OUTPTR_ARGS_Helper(OutInPtr&& outInPtr)
+template <class OutInPtr> OutInPtr&& IID_OUTPTR_ARGS_Helper(OutInPtr&& outInPtr)
 {
     // do compile-time checks on pointer type
     using Pointer = typename OutInPtrTraits<OutInPtr>::Pointer;
@@ -38,13 +35,9 @@ OutInPtr&& IID_OUTPTR_ARGS_Helper(OutInPtr&& outInPtr)
     return std::forward<OutInPtr>(outInPtr);
 }
 
-
 struct IUnknownDeleter
 {
-    inline void operator()(IUnknown* ptr) const noexcept
-    {
-        ptr->Release();
-    }
+    inline void operator()(IUnknown* ptr) const noexcept { ptr->Release(); }
 };
 
 } // namespace Detail
@@ -52,12 +45,11 @@ struct IUnknownDeleter
 // Smart pointer that holds ownership of the COM object
 // Will Release() the COM object when going out of scope or on re-assignment
 // To relinquish ownership and return to manual lifetime management, call .release()
-template<typename T>
-using ComPtr = std::unique_ptr<T, Detail::IUnknownDeleter>;
+template <typename T> using ComPtr = std::unique_ptr<T, Detail::IUnknownDeleter>;
 
 } // namespace Util
 
 // Compare combaseapi.h IID_PPV_ARGS
-#define IID_OUTPTR_ARGS(outInPtr) \
-    __uuidof(typename Util::Detail::OutInPtrTraits<decltype(outInPtr)>::Pointer), \
-    Util::Detail::IID_OUTPTR_ARGS_Helper(outInPtr)
+#define IID_OUTPTR_ARGS(outInPtr)                                                                                      \
+    __uuidof(typename Util::Detail::OutInPtrTraits<decltype(outInPtr)>::Pointer),                                      \
+        Util::Detail::IID_OUTPTR_ARGS_Helper(outInPtr)
